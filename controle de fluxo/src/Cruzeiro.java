a9d602c1f2e441049e9ccbffcfeb7a6d5cfb53a5
FelipeMeirelesDev <felipegoncalvesmeireles2003@gmail.com>
2024-07-07 22:58:56 -0300

    Exemplo While

---
+++ controle de fluxo/src/Cruzeiro.java
@@ -0,0 +1,13 @@
+public class Cruzeiro {
+    public static void main(String[] args) {
+        int golMatheusPereira = 1;
+        int golAlvaroBarreal = 1;
+        int golGabrielVeron = 1;
+        int totaldegols = golMatheusPereira + golAlvaroBarreal + golGabrielVeron;
+
+        if (totaldegols >= 3) {
+            System.out.println("Goleada do Cruzeiro PRRAAAAA!!!!!");
+        }
+
+    }
+}
